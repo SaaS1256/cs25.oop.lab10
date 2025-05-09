d57649a97b583d7b697cf2bca649f203711577f7
Khishigbayar B222230046 <163809928+SaaS1256@users.noreply.github.com>
2025-05-09 12:19:11 +0800

    Update Zoo.java

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,40 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 abstract class Animal {
-	protected String name;
+    protected String name;
 
-	public Animal(String name) {
-		this.name = name;
-	}
+    public Animal(String name) {
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 }
 
 class Mammal extends Animal {
-	public Mammal(String name) {
-		super(name);
-	}
+    public Mammal(String name) {
+        super(name);
+    }
 }
 
 class Bird extends Animal {
-	public Bird(String name) {
-		super(name);
-	}
+    public Bird(String name) {
+        super(name);
+    }
 }
 
 public class Zoo<T extends Animal> {
-	private List<T> animals = new ArrayList<>();
+    private List<T> animals = new ArrayList<>();
 
-	public void addAnimal(T animal) {
-		animals.add(animal);
-	}
+    public void addAnimal(T animal) {
+        animals.add(animal);
+    }
 
-	// Хэрэгжүүл: transferAnimals
-	public void transferAnimals(List<? extends T> source, List<? super T> destination) {
-		// Бүх амьтдыг source-оос destination руу шилжүүл
-	}
+    public void transferAnimals(List<? extends T> source, List<? super T> destination) {
+        for (T animal : source) {
+            destination.add(animal);
+        }
+    }
 
-	public List<T> getAnimals() {
-		return animals;
-	}
+    public List<T> getAnimals() {
+        return animals;
+    }
+}
+
+public class ZooApp {
+    public static void main(String[] args) {
+        Zoo<Mammal> mammalZoo = new Zoo<>();
+        mammalZoo.addAnimal(new Mammal("Арслан"));
+        
+        Zoo<Bird> birdZoo = new Zoo<>();
+        birdZoo.addAnimal(new Bird("Тогоруу"));
+        
+        List<Animal> generalZoo = new ArrayList<>();
+        
+        mammalZoo.transferAnimals(mammalZoo.getAnimals(), generalZoo);
+        birdZoo.transferAnimals(birdZoo.getAnimals(), generalZoo);
+        
+        generalZoo.forEach(a -> System.out.println(a.getName()));
+    }
 }
