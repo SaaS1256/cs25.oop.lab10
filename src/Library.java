918c542f5c95785d7ea0f0a8c5165a616d5c1656
Khishigbayar B222230046 <163809928+SaaS1256@users.noreply.github.com>
2025-05-09 12:24:20 +0800

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -1,45 +1,50 @@
 import java.util.*;
 
 class Book {
-	private String title;
-	private int year;
+    private String title;
+    private int year;
 
-	public Book(String title, int year) {
-		this.title = title;
-		this.year = year;
-	}
+    public Book(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public int getYear() {
-		return year;
-	}
+    public int getYear() {
+        return year;
+    }
 }
 
 public class Library {
-	private List<Book> books = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
 
-	public void addBook(String title, int year) {
-		books.add(new Book(title, year));
-	}
+    public void addBook(String title, int year) {
+        books.add(new Book(title, year));
+    }
 
-	class BookShelf implements Iterable<Book> {
-		private int minYear;
+    class BookShelf implements Iterable<Book> {
+        private int minYear;
 
-		public BookShelf(int minYear) {
-			this.minYear = minYear;
-		}
+        public BookShelf(int minYear) {
+            this.minYear = minYear;
+        }
 
-		@Override
-		public Iterator<Book> iterator() {
-			// Хэрэгжүүл: minYear-с хойшхи номнуудыг буцаа
-			return null; // Оруул
-		}
-	}
+        @Override
+        public Iterator<Book> iterator() {
+            List<Book> filteredBooks = new ArrayList<>();
+            for (Book b : books) {
+                if (b.getYear() >= minYear) {
+                    filteredBooks.add(b);
+                }
+            }
+            return filteredBooks.iterator();
+        }
+    }
 
-	public BookShelf getRecentBooks(int minYear) {
-		return new BookShelf(minYear);
-	}
+    public BookShelf getRecentBooks(int minYear) {
+        return new BookShelf(minYear);
+    }
 }
