122ab84a216821f0db7ad1ba77752909e4d23df0
Khishigbayar B222230046 <163809928+SaaS1256@users.noreply.github.com>
2025-05-09 12:20:04 +0800

    Update StringProcessor.java

--- src/StringProcessor.java
+++ src/StringProcessor.java
@@ -2,15 +2,25 @@ import java.util.*;
 import java.util.function.*;
 
 public class StringProcessor {
-	public static void processStrings(List<String> strings,
-			Predicate<String> filter,
-			Function<String, String> transformer,
-			Consumer<String> printer) {
-		// Хэрэгжүүл: filter, transform, print хий
-	}
+    public static void processStrings(List<String> strings,
+                                      Predicate<String> filter,
+                                      Function<String, String> transformer,
+                                      Consumer<String> printer) {
+        for (String s : strings) {
+            if (filter.test(s)) {
+                String transformed = transformer.apply(s);
+                printer.accept(transformed);
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
-		// Хэрэгжүүл: Predicate, Function, Consumer-ийг тодорхойлж, processStrings дууд
-	}
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Java", "Програмчлал", "Код");
+
+        Predicate<String> filter = word -> word.length() > 5;
+        Function<String, String> transformer = word -> new StringBuilder(word).reverse().toString();
+        Consumer<String> printer = word -> System.out.println("ГАРШИЛ: " + word);
+
+        processStrings(words, filter, transformer, printer);
+    }
 }
